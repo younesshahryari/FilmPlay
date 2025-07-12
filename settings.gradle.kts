fb1a43956b625d455feb5912ec4998ffcb267c7e
Younes Shahryari <youneshah@gmail.com>
2025-07-12 10:38:27 +0330

    Rename app from InterviewPlay to FilmPlay
    The application name displayed to the user has been changed from "InterviewPlay" to "FilmPlay". This update is reflected in the `strings.xml` resource file.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,7 +19,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "AparatAndroidInterview"
+rootProject.name = "FilmPlay"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 include(":app")
 include(":core:data")
